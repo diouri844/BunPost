f15f7e9fbb27ad16110f172e7d46abbc4ce5505a
Chopen <75852177+diouri844@users.noreply.github.com>
2023-10-14 23:24:30 +0200

    Update index entry point

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import { Elysia } from "elysia";
 import { setupPostRoutes } from "./routes/post.route";
 // import db class : 
 
-import BunDatabase from "./database/db";
+import DbInstance from "./database/db";
 
 const app = new Elysia();
 // setup db : 
-app.decorate('db', new BunDatabase());
+app.decorate('db', DbInstance);
 
 
 // setup Ping tester : 
@@ -15,6 +15,7 @@ app.decorate('db', new BunDatabase());
 
 
   app.listen("3000",()=>{
+    
     console.log(
       `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
     );
