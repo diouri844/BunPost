b1adf16a5692faa13a7053ffce53b9a52043b07e
Chopen <75852177+diouri844@users.noreply.github.com>
2023-10-18 14:11:15 +0200

    Update post Routes

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -5,8 +5,10 @@ import { AddPost, GetPost, GetPostList } from "../controller/post.controller";
 
 export function setupPostRoutes(app: Elysia) {
   app.get("/api/posts", GetPostList);
-
   app.post("/api/posts", AddPost);
   app.get("/api/post/:postId", GetPost);
+
+
+  
   // Add more user-related routes as needed
 }
