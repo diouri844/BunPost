de4fdff037b5ac1bb44e09b92a2d638ca307aa94
Chopen <75852177+diouri844@users.noreply.github.com>
2023-10-18 22:56:25 +0200

    Update Post routes

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,14 +1,16 @@
 import { Elysia, Context } from "elysia";
 
 // import controllers : 
-import { AddPost, GetPost, GetPostList } from "../controller/post.controller";
+import { AddPost, GetPost, GetPostList, deletePost, getPostByAuthor } from "../controller/post.controller";
 
 export function setupPostRoutes(app: Elysia) {
   app.get("/api/posts", GetPostList);
+  app.get("/api/posts/author",getPostByAuthor);
   app.post("/api/posts", AddPost);
   app.get("/api/post/:postId", GetPost);
+  app.delete("/api/post/:postId", deletePost);
 
 
-  
+
   // Add more user-related routes as needed
 }
