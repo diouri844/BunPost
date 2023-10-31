f360e8e9e63a4c6704932cfe1759a3186d00229f
Chopen <75852177+diouri844@users.noreply.github.com>
2023-10-31 22:07:50 +0100

    Update author model

--- src/models/Author.model.ts
+++ src/models/Author.model.ts
@@ -6,18 +6,41 @@ import IAuthor from "../interfaces/author.interface";
 // define my Author class provider :
 
 class AuthorProvider{
-    static async getAuthorById(authorId:Number): Promise<IAuthor>{
-        const fetchResult = myDbInstance.getDb().run(
-            `GET * FROM Author WHERE id = ${authorId}`
-        );
-        console.log( fetchResult );
-        const author:IAuthor = {
-            id:0,
-            name:"test",
-            contact:"string"
-        };
+    static async getAuthorById(authorId:string): Promise<IAuthor|String>{
+        let author:IAuthor|String = "";
+        const fetchResult = await myDbInstance.
+        getDb()
+        .query(
+            'SELECT * FROM Author WHERE id = ?',
+        ).get(authorId);
+        if ( !fetchResult ){
+            author = "Not found";
+        }else{
+            author = fetchResult;
+        }
         return author;
+    };
+
+    static async getAuthorList(): Promise<IAuthor[]>{
+        let authorList:IAuthor[] = [];
+        // fetch all the author in the database: 
+        const fetchResult =  myDbInstance.getDb().query("SELECT * FROM Author").values();
+        // map over all the author: 
+        fetchResult.forEach(
+            item => {
+                const author:IAuthor = {
+                    id:Number(item[0]),
+                    publicName:String(item[1]),
+                    fullName:String(item[2]),
+                    contact:String(item[3])
+                }
+                authorList.push(author);
+            }
+        );
+        return authorList;
     }
+
+
 } 
 
 
