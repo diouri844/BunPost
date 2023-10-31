86e8a741807df981ec7fb22a3b5db736c0533a3e
Chopen <75852177+diouri844@users.noreply.github.com>
2023-10-31 20:07:45 +0100

    Update entry point

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 import { Elysia } from "elysia";
 
 import { setupPostRoutes } from "./routes/post.route";
+import setupAuthorRoutes from "./services/author.service";
 // import db class : 
-
-import DbInstance from "./database/db";
+import myDbInstance from "./database/setupDb";
 
 const app = new Elysia();
-// setup db : 
-app.decorate('db', DbInstance);
 
 
 // setup Ping tester : 
-  app.use(setupPostRoutes);
+//app.use(setupPostRoutes);
+app.use(setupAuthorRoutes);
 
-
-  app.listen("3000",()=>{
-    
+  app.listen("3000",
+  async ()=>{
+    // try to connect to the db : 
+    myDbInstance.ping();
     console.log(
       `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
     );
