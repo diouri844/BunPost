08eb60afcdf5bd70769076a1573b88cc6f2bc060
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-04 20:10:08 +0100

    Update post  model

--- src/models/Post.model.ts
+++ src/models/Post.model.ts
@@ -1,37 +1,11 @@
 
-import Burm, { DataType } from "burm";
 import IPost from "../interfaces/post.interface";
 
 
-const PostModel = Burm.register<IPost>(
-    "Post",
-    [
-        {
-            field: "id",
-            type: DataType.INTEGER,
-            primary: true,
-        },
-        {
-            field: "title",
-            type: DataType.STRING,
-            required:true
-        },
-        {
-            field: "content",
-            type: DataType.STRING,
-            required:true
-        },
-        {
-            field: "authorId",
-            type: DataType.INTEGER,
-            required:true,
-            ref:"Author"            
-        }
-    ]
-);
-
-export default PostModel;
-
-
+export default class PostProvider {
+    static async getPostById(postId: string): Promise<IPost|string>{
+        return "";
+    };
+}
 
 
