c039a240cc9cd8e018248c4a1999d97ff8d37d52
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-13 21:28:37 +0100

    Update post controller

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -3,6 +3,7 @@ import { Context } from "elysia";
 // define the post controller as callback function:
 import IPost from "../interfaces/post.interface";
 import PostProvider from "../models/Post.model";
+import { STATUS_CODES } from "http";
 
 
 
@@ -20,13 +21,12 @@ export default class PostController{
         let authorPostList:IPost[] = [];
         return authorPostList;
     };
-    async getPostById(context:Context):Promise<IPost>{
-        const target:IPost = {
-            content: "",
-            type: "",
-            author: ""
-        };
-        return target;
+    async getPostById(context:Context, response:Response){
+        // extract postid from request context : 
+        const { postId } = context.params;
+        console.log(postId);
+        const fetchResult:IPost|string = await PostProvider.getPostById(String(postId));
+        return ({ data: fetchResult});
     };
     async getPostList():Promise<IPost[]>{
         const result:IPost[] = await PostProvider.getPostList();
