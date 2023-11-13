f516d61120ac3da108cdcbeca4b3355d667d7aca
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-13 09:10:28 +0100

    Update post  model

--- src/models/Post.model.ts
+++ src/models/Post.model.ts
@@ -1,11 +1,34 @@
-
 import IPost from "../interfaces/post.interface";
+import IAuthor from "../interfaces/author.interface";
+import myDbInstance from "../database/setupDb";
+
 
 
 export default class PostProvider {
     static async getPostById(postId: string): Promise<IPost|string>{
         return "";
     };
+    static async getPostList():Promise<IPost[]>{
+        let postList:IPost[] = [];
+        const fetchresult = await myDbInstance.getDb().query(
+            "SELECT * FROM Post"
+        ).values();
+        // cast data : 
+        console.log( fetchresult );
+        fetchresult.forEach(
+            item=>{
+                const post:IPost = {
+                    id:Number(item[0]),
+                    content:String(item[1]),
+                    type:String(item[2]),
+                    author:String(item[3])
+                };
+                postList.push(post);
+            }
+        );
+        return postList;
+    }
+
 }
 
 
