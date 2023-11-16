8cf71ec80e2d28a8d3d3f03b40878fa4fd56ed16
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-16 21:01:45 +0100

    Update post router

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -19,7 +19,7 @@ export default class PostRoutes {
     this.app.get(PREFIXER+"/author/:authorId",PostProvider.getPostByAuthor);
     this.app.post(PREFIXER+"/author/:authorId", PostProvider.AddPost);
     this.app.get(PREFIXER+"/:postId", PostProvider.getPostById);
-    this.app.delete(PREFIXER+"/:postId", ()=>{});
+    this.app.delete(PREFIXER+"/:postId", PostProvider.deletePost);
     return;
   };
 }
