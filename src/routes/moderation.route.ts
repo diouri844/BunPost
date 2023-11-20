c19adce696ed3a9b456f9b6cea15ea88bc41952e
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-20 10:23:30 +0100

    Update moderation routes

--- src/routes/moderation.route.ts
+++ src/routes/moderation.route.ts
@@ -1,4 +1,5 @@
 import { Elysia } from "elysia";
+import ModerationController from "../controller/moderation.controller";
 
 
 const PREFIXER:string = "/api/moderate";
@@ -11,7 +12,12 @@ export default class ModerationRoutes {
         this.setupRoutes();
     }
     private setupRoutes():void{
-        this.app.get(PREFIXER, ()=>{});
+        // setup an instance for the controller : 
+        const ModerateProvider = new ModerationController();
+        this.app.get(
+            PREFIXER+"/Moderate/<moderate_id>/<state>", 
+            ModerateProvider.ModeratePost
+        );
         this.app.get(PREFIXER+"/author/:authorId",()=>{});
         this.app.get(PREFIXER+"/:postId", ()=>{});
         return;
