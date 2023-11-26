5aa2ea896bb6e01071b920176f714281e36f5fc0
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-26 19:51:31 +0100

    Update moderate controller

--- src/controller/moderation.controller.ts
+++ src/controller/moderation.controller.ts
@@ -7,6 +7,8 @@ import PostProvider from "../models/Post.model";
 import ModerateProvider from "../models/Moderation.model";
 import IModerate from "../interfaces/moderate.interface";
 import IPost from "../interfaces/post.interface";
+import AuthorProvider from "../models/Author.model";
+import IAuthor from "../interfaces/author.interface";
 
 
 export default class ModerationController {
@@ -193,6 +195,55 @@ export default class ModerationController {
                 message:"Failed to delete item"
             }
         };
+    }
+    async BlockAuthor( context: Context):Promise<any>{
+        //extract the author id from the context params :
+        const { authorId } = context.params;
+        // check if the author is already exists or not :
+        const authorTarget:IAuthor|String = await AuthorProvider.getAuthorById(authorId);
+        if ( typeof authorTarget === 'string'){
+            return {
+                message:"Author not found"
+            };
+        }
+        // get the list of moderation belong to the author and make them all not improved and not published:
+        const result:string[] = await ModerateProvider.getModerateByAuthor(authorId);
+        // to completed later :
+        return;
+    }
+
+
+    async CreateNewModeration( context: Context ): Promise<any>{
+        // extract the author and the post id from context params :
+        const { authorId, postId } = context.params;
+        // check if the post is already exist :
+        const fetchPost:IPost|string = await PostProvider.getPostById(postId);
+        if ( typeof fetchPost === 'string'){
+            return {
+                message:"Post not found"
+            };
+        }
+        // extract the author id from the post :
+        const authorTarget:string = fetchPost.author;
+        if( authorId !== authorTarget ){
+            return {
+                message:"Post author not found"
+            };
+        }
+        // extract the extra payload :
+        const improved:boolean = context.body.improved||false;
+        const published:boolean = context.body.published||false;
+
+        // call static service method:
+        
+        
+        return {
+            postId,
+            authorId,
+            improved,
+            published
+        };
+
     }
 
 
