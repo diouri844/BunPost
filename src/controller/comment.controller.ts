37bdf5e4064a083d76ebc99bd52fcf7775f00271
Chopen <75852177+diouri844@users.noreply.github.com>
2023-12-05 20:06:34 +0100

    Update comment controller

--- src/controller/comment.controller.ts
+++ src/controller/comment.controller.ts
@@ -46,4 +46,50 @@ export default class CommentController {
             data: replyComments
         } 
     }
+    // add new comment ( comment to a post ):
+    async addComment( context:Context ):Promise<any>{
+        // extract the post id from the context object :
+        const { postId } = context.params;
+        // check if the post exists or not :
+        const postResult:IPost|string = await PostProvider.getPostById(postId);
+        if( typeof postResult === "string" ){
+            return {
+                message:"Post not found"
+            }
+        }
+        // extract the needed parameters from the context body:
+        const authorId:string = context.body!.authorId;
+        const body:string = context.body!.body;
+        // check if authorId and body are valid :
+        if (!authorId || !body ){
+            return {
+                message:"Invalid author id or body "
+            }
+        } 
+        // now the post exist let add comment to it :
+        const CommentAddResult:string|number = await CommentProvider.addCommentToPost(
+            postId,authorId,body
+        );
+        if ( typeof CommentAddResult === "string" ){
+            return {
+                message : CommentAddResult
+            }
+        }
+        return {
+            message: "Comment added successfully"
+        }
+    }
+    // add comment to comment : ( reply to comment by id)
+    async replyToComment( context:Context ):Promise<any>{
+        // extract the post id from the context object :
+        const { commentId } = context.params;
+        // check if the post exists or not :
+        const commentResult:IComment|string = await CommentProvider.getCommentById( commentId);
+        if( typeof commentResult === "string" ){
+            return {
+                message:"Comment not found"
+            }
+        }
+        // make it done later :
+    }
 };
