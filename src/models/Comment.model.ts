87feb25519c85e2cd0b2f8062bf1a49b02054eab
Chopen <75852177+diouri844@users.noreply.github.com>
2023-12-06 20:20:13 +0100

    update comment model

--- src/models/Comment.model.ts
+++ src/models/Comment.model.ts
@@ -1,3 +1,4 @@
+import { cast } from "sequelize";
 import myDbInstance from "../database/setupDb";
 import IComment from "../interfaces/comment.inetrface";
 
@@ -29,7 +30,7 @@ export default class CommentProvider {
     static async getCommentsReplys(commentId:string): Promise<IComment[]>{
         let CommentList:IComment[] = [];
         const fetchResult = await myDbInstance.getDb().prepare(
-            "SELECT * FROM CommentList WHERE parentId = ? "
+            "SELECT * FROM Comment WHERE parentId = ? "
         ).all(commentId);
         // fetch over respose and push them to the result list :
         fetchResult.forEach(
@@ -73,4 +74,58 @@ export default class CommentProvider {
             return " Failed to add comment ";
         }
     };
+    // static method to add comment to comment :
+    static async addCommentToComment(
+        parenttId:string,
+        authorId:string,
+        body:string
+    ):Promise<string|number>{
+        try{
+            // create new date and parse is as an string :
+            const strDate = String(new Date());
+            // insert new comment :
+            await myDbInstance.getDb()
+            .prepare(
+                "Insert into Comment Values (Null,?,?,?,?,?)"
+            ).run(
+                body,strDate,parenttId,0,authorId
+            );
+            // get the inserted comment id :
+            const lastInsertId = myDbInstance.getDb().query('SELECT last_insert_rowid() as id').values()[0][0];
+            return Number(lastInsertId);
+        }catch( error ){
+            console.log( error );
+            return " Failed to add comment ";
+            
+        }
+    }
+    // update comment ( edit body only ):
+    static async updateComment( commentId:string ,body:string ):Promise<string|IComment>{
+        // get the comment by id and update body :
+        try{
+            await myDbInstance.getDb().prepare(
+                "UPDATE Comment SET body = ? WHERE id = ?"
+            ).run(body,commentId);
+            // comment body updated successfully , let get the new comment:
+            const target = await myDbInstance.getDb().
+            prepare("SELECT * FROM Comment WHERE id = ?").
+            get(commentId);
+            return target as IComment;
+        }catch( error ){
+            console.log( error );
+            return "Can not update comment ";
+        }
+    }
+    //static delete comment method :
+    static async deleteComment ( commentId : string ):Promise<boolean> {
+        try{
+            myDbInstance.getDb().prepare(
+                "DELETE FROM Comment WHERE id = ?"
+            ).run(commentId);
+            return true;
+        }catch( error ){
+            console.log( error );
+            return false;
+        }
+    }
 }
